1b143fa4dc7089c88ef3ef51f0126b25e4b4ee80
souzafilhoisac <souzafilhoisac@gmail.com>
2022-05-26 18:51:42 -0300

    Reordenando projeto

--- assets/script.js
+++ assets/script.js
@@ -9,15 +9,15 @@ var segundos = data.getSeconds()
 tempo = setInterval(carregar)
     if (hora >= 0 && hora < 12) {
         msg.innerHTML = `<strong>Bom dia! São: ${hora}h ${minuto}min ${segundos} segundos</strong>`
-        img.src = 'manha.png'
+        img.src = 'assets/manha.png'
         document.body.style.background = '#fdda45'
     } else if (hora >= 12 && hora < 18) {
         msg.innerHTML = `<strong>Boa tarde! São: ${hora}h ${minuto}min ${segundos} segundos</strong>`
-        img.src = 'tarde.png'
+        img.src = 'assets/tarde.png'
         document.body.style.background = '#d86615'
     } else {
         msg.innerHTML = `<strong>Boa noite! São: ${hora}h ${minuto}min ${segundos} segundos</strong>`
-        img.src = 'noite.png'
+        img.src = 'assets/noite.png'
         document.body.style.background = '#021623'
     }
 }
